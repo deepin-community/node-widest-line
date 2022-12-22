91a6da8e62996b301f1aeaae07a7bc64ab6ab218
Deepin Developer <deepin-dev@deepin.org>
2022-12-22 14:17:11 +0800

    feat: Init commit
    init node-widest-line
    
    Log:

---
+++ test.js
@@ -0,0 +1,8 @@
+import test from 'ava';
+import widestLine from '.';
+
+test('main', t => {
+	t.is(widestLine('a'), 1);
+	t.is(widestLine('a\nbe'), 2);
+	t.is(widestLine('古\n\u001B[1m@\u001B[22m'), 2);
+});
